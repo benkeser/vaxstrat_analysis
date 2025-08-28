9fe56149c388eea5a09168c906accf9805c3d406
Allison Codi <allison.codi@emory.edu>
2025-08-28 10:34:45 -0400

    redo bound power

--- simulations/plot_power.R
+++ simulations/plot_power.R
@@ -7,7 +7,9 @@
 # for each combo at each n, calc power, make df
 # use df to make power plot
 
-library(scales)  # for percent_format()
+library(dplyr)
+library(scales) 
+library(ggplot2)
 
 here::i_am("plot_power.R")
 
@@ -45,10 +47,20 @@ for(i in 1:nrow(combo_grid)){
                                  results_grid$inflation == combo_grid$inflation[i]] <-
       mean(as.numeric(sub_res_n$nat_inf_reject == TRUE))
     
+    results_grid$nat_inf_bound_power[results_grid$n == n &
+                                 results_grid$effect_protect == combo_grid$effect_protect[i] &
+                                 results_grid$inflation == combo_grid$inflation[i]] <-
+      mean(as.numeric(sub_res_n$nat_inf_upper_bound_reject.additive == TRUE), na.rm = TRUE) # some NAs w small sample sizes?? na.rm = true temp, look into later
+    
     results_grid$doomed_power[results_grid$n == n &
                                 results_grid$effect_protect == combo_grid$effect_protect[i] &
                                 results_grid$inflation == combo_grid$inflation[i]] <-
       mean(as.numeric(sub_res_n$doomed_reject == TRUE))
+    
+    results_grid$doomed_bound_power[results_grid$n == n &
+                                results_grid$effect_protect == combo_grid$effect_protect[i] &
+                                results_grid$inflation == combo_grid$inflation[i]] <-
+      mean(as.numeric(sub_res_n$doomed_upper_bound_reject.additive == TRUE), na.rm = TRUE)
     
     results_grid$pop_power[results_grid$n == n &
                              results_grid$effect_protect == combo_grid$effect_protect[i] &
@@ -61,14 +73,20 @@ for(i in 1:nrow(combo_grid)){
 results_grid <- results_grid %>%
   mutate(
     effect_protect = if_else(effect_protect == TRUE, "Protected effect < 0", "Protected effect = 0"),
-    effect_doomed  = if_else(inflation < 0, "Doomed effect < 0", "Doomed effect = 0")
+    effect_doomed  = if_else(inflation == 0, "Doomed effect = 0", if_else(inflation == -0.5, "Doomed effect < 0\n(Inflation = -0.5)", "Doomed effect < 0\n(Inflation = -1)"))
   )
+
+results_grid$effect_doomed <- factor(results_grid$effect_doomed, levels = c("Doomed effect < 0\n(Inflation = -1)",
+                                                                            "Doomed effect < 0\n(Inflation = -0.5)",
+                                                                            "Doomed effect = 0"))
 
 # Plot power in percent with Lancet color scheme
 ggplot(results_grid, aes(x = n, y = nat_inf_power * 100, color = "Naturally Infected")) +
   geom_line(size = 1.2) +
   geom_line(aes(y = doomed_power * 100, color = "Doomed"), size = 1.2) +
   geom_line(aes(y = pop_power * 100, color = "Population"), size = 1.2) +
+  geom_line(aes(y = nat_inf_bound_power * 100, color = "Naturally Infected"), linetype = 2, size = 1.2) + 
+  geom_line(aes(y = doomed_bound_power * 100, color = "Doomed"), linetype = 2, size = 1.2) + 
   facet_grid(effect_protect ~ effect_doomed) +
   scale_color_manual(
     name = "Estimand",
