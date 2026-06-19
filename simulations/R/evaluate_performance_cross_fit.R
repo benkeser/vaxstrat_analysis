bd1068bcbea216bbe3dd8dea94150604581b8f59
Allison Codi <allison.codi@emory.edu>
2026-06-19 09:23:54 -0400

    eval cross fit

---
+++ simulations/R/evaluate_performance_cross_fit.R
@@ -0,0 +1,188 @@
+# ------------------------------------------------------------------------------
+# Script to evaluate performance of generic simulations 
+# ------------------------------------------------------------------------------
+
+# Path to installed packages on cluster
+options(echo = TRUE)
+
+.libPaths(c("/apps/R/4.4.0/lib64/R/site/library","/apps/R/4.4.0/lib64/R/library", "~/Rlibs_ve_trial"))
+
+here::i_am("R/evaluate_performance_simulation_1.R")
+
+source(here::here("R/simulate_data.R"))
+source(here::here("R/get_truth.R"))
+
+devtools::load_all("~/vaxstrat")
+
+library(dplyr)
+
+# Path to projects folder where results will be saved
+project_dir <- "/projects/dbenkes/allison/vegrowth_analysis/results/cross_fit/"
+
+# command args config setting
+cargs <- commandArgs(trailingOnly = TRUE)
+setting <- cargs[[1]]
+
+cfg <- yaml::read_yaml("config_cross_fit.yml")
+config <- cfg[[setting]]
+
+result_dir <- file.path(project_dir, setting)
+
+# Get true effects
+truth_file <- here::here(paste0("truth/", setting, "_truth.Rds"))
+if(!file.exists(truth_file)){
+  truth <- get_truth_cross_fit(config)
+  saveRDS(truth, truth_file)
+} else{
+  truth <- readRDS(truth_file)
+}
+
+# List all matching .Rds files
+files <- list.files(
+  path = result_dir,
+  pattern = paste0("^", setting, "_overall_seed_.*\\.Rds$"),
+  full.names = TRUE
+)
+
+all_result_df <- data.frame()
+
+for(fname in files){
+  res_obj <- readRDS(fname)
+  seed <- as.integer(gsub(paste0(setting, "_overall_seed_|\\.Rds"), "", basename(fname)))
+  
+  grid <- expand.grid(seed = seed, 
+                      n_sample_size = as.numeric(config$n_sample_size),
+                      effect_protect = config$effect_protect,
+                      doomed_inflation = as.numeric(config$doomed_inflation),
+                      protected_epsilon = as.numeric(config$protected_epsilon),
+                      doomed_epsilon = as.numeric(config$doomed_epsilon),
+                      immune_epsilon = as.numeric(config$immune_epsilon))
+  
+  for(i in 1:nrow(grid)){
+    
+    res <- res_obj[[i]]
+    
+    result_df <- data.frame(
+      seed = seed,
+      n = grid$n_sample_size[i],
+      effect_protect = grid$effect_protect[i],
+      doomed_inflation = grid$doomed_inflation[i],
+      protected_epsilon = grid$protected_epsilon[i],
+      dooomed_epsilon = grid$doomed_epsilon[i],
+      immune_epsilon = grid$immune_epsilon[i],
+      estimand = c(rep("nat_inf",3)),
+      method = c("aipw_CW", "aipw_ER", "aipw_ER_CW"),
+      # Additive estimate and confidence interval
+      additive_estimate = c(
+        res$nat_inf$aipw_CW$pt_est['additive_effect'],
+        res$nat_inf$aipw_ER$pt_est['additive_effect'],
+        res$nat_inf$aipw_ER_CW$pt_est['additive_effect']
+      ),
+      additive_lower_ci = c(
+        res$nat_inf$aipw_CW$pt_est['additive_effect'] - 1.96*res$nat_inf$aipw_CW$pt_est['additive_se'],
+        res$nat_inf$aipw_ER$pt_est['additive_effect'] - 1.96*res$nat_inf$aipw_ER$pt_est['additive_se'],
+        res$nat_inf$aipw_ER_CW$pt_est['additive_effect'] - 1.96*res$nat_inf$aipw_ER_CW$pt_est['additive_se']),
+      additive_upper_ci = c(
+        res$nat_inf$aipw_CW$pt_est['additive_effect'] + 1.96*res$nat_inf$aipw_CW$pt_est['additive_se'],
+        res$nat_inf$aipw_ER$pt_est['additive_effect'] + 1.96*res$nat_inf$aipw_ER$pt_est['additive_se'],
+        res$nat_inf$aipw_ER_CW$pt_est['additive_effect'] + 1.96*res$nat_inf$aipw_ER_CW$pt_est['additive_se']),
+      # Multiplicative estimate and confidence interval
+      mult_estimate = c(
+        c(exp(as.numeric(res$nat_inf$aipw_CW$pt_est['log_multiplicative_effect'])),
+          exp(as.numeric(res$nat_inf$aipw_ER$pt_est['log_multiplicative_effect'])),
+          exp(as.numeric(res$nat_inf$aipw_ER_CW$pt_est['log_multiplicative_effect'])))),
+      mult_lower_ci = c(
+        exp(res$nat_inf$aipw_CW$pt_est['log_multiplicative_effect'] - 1.96*res$nat_inf$aipw_CW$pt_est['log_multiplicative_se']),
+        exp(res$nat_inf$aipw_ER$pt_est['log_multiplicative_effect'] - 1.96*res$nat_inf$aipw_ER$pt_est['log_multiplicative_se']),
+        exp(res$nat_inf$aipw_ER_CW$pt_est['log_multiplicative_effect'] - 1.96*res$nat_inf$aipw_ER_CW$pt_est['log_multiplicative_se'])),
+      mult_upper_ci = c(
+        exp(res$nat_inf$aipw_CW$pt_est['log_multiplicative_effect'] + 1.96*res$nat_inf$aipw_CW$pt_est['log_multiplicative_se']),
+        exp(res$nat_inf$aipw_ER$pt_est['log_multiplicative_effect'] + 1.96*res$nat_inf$aipw_ER$pt_est['log_multiplicative_se']),
+        exp(res$nat_inf$aipw_ER_CW$pt_est['log_multiplicative_effect'] + 1.96*res$nat_inf$aipw_ER_CW$pt_est['log_multiplicative_se'])),
+      # NEW psi_1 and psi_0
+      psi_1 = c(
+        res$nat_inf$aipw_CW$pt_est['psi_1'],
+        res$nat_inf$aipw_ER$pt_est['psi_1'],
+        res$nat_inf$aipw_ER_CW$pt_est['psi_1']),
+      psi_1_lower_ci = c(
+        res$nat_inf$aipw_CW$pt_est['psi_1'] - 1.96*res$nat_inf$aipw_CW$pt_est['se_psi_1'],
+        res$nat_inf$aipw_ER$pt_est['psi_1'] - 1.96*res$nat_inf$aipw_ER$pt_est['se_psi_1'],
+        res$nat_inf$aipw_ER_CW$pt_est['psi_1'] - 1.96*res$nat_inf$aipw_ER_CW$pt_est['se_psi_1']),
+      psi_1_upper_ci = c(
+        res$nat_inf$aipw_CW$pt_est['psi_1'] + 1.96*res$nat_inf$aipw_CW$pt_est['se_psi_1'],
+        res$nat_inf$aipw_ER$pt_est['psi_1'] + 1.96*res$nat_inf$aipw_ER$pt_est['se_psi_1'],
+        res$nat_inf$aipw_ER_CW$pt_est['psi_1'] + 1.96*res$nat_inf$aipw_ER_CW$pt_est['se_psi_1']),
+      psi_0 = c(
+        res$nat_inf$aipw_CW$pt_est['psi_0'],
+        res$nat_inf$aipw_ER$pt_est['psi_0'],
+        res$nat_inf$aipw_ER_CW$pt_est['psi_0']),
+      psi_0_lower_ci = c(
+        res$nat_inf$aipw_CW$pt_est['psi_0'] - 1.96*res$nat_inf$aipw_CW$pt_est['se_psi_0'],
+        res$nat_inf$aipw_ER$pt_est['psi_0'] - 1.96*res$nat_inf$aipw_ER$pt_est['se_psi_0'],
+        res$nat_inf$aipw_ER_CW$pt_est['psi_0'] - 1.96*res$nat_inf$aipw_ER_CW$pt_est['se_psi_0']),
+      psi_0_upper_ci = c(
+        res$nat_inf$aipw_CW$pt_est['psi_0'] + 1.96*res$nat_inf$aipw_CW$pt_est['se_psi_0'],
+        res$nat_inf$aipw_ER$pt_est['psi_0'] + 1.96*res$nat_inf$aipw_ER$pt_est['se_psi_0'],
+        res$nat_inf$aipw_ER_CW$pt_est['psi_0'] + 1.96*res$nat_inf$aipw_ER_CW$pt_est['se_psi_0']),
+      # Truth
+      additive_truth = c(rep(truth$effect_nat_inf[truth$doomed_inflation == grid$doomed_inflation[i] & truth$protected_epsilon == grid$protected_epsilon[i] & truth$doomed_epsilon == grid$doomed_epsilon[i] & truth$immune_epsilon == grid$immune_epsilon[i]], 3)),
+      mult_truth = c(rep(truth$effect_nat_inf_mult[truth$doomed_inflation == grid$doomed_inflation[i] & truth$protected_epsilon == grid$protected_epsilon[i] & truth$doomed_epsilon == grid$doomed_epsilon[i] & truth$immune_epsilon == grid$immune_epsilon[i]], 3)),
+      
+      psi_1_truth = c(rep(truth$E_Y1__protected_or_doomed[truth$doomed_inflation == grid$doomed_inflation[i] & truth$protected_epsilon == grid$protected_epsilon[i] & truth$doomed_epsilon == grid$doomed_epsilon[i] & truth$immune_epsilon == grid$immune_epsilon[i]], 3)), 
+      psi_0_truth = c(rep(truth$E_Y0__protected_or_doomed[truth$doomed_inflation == grid$doomed_inflation[i] & truth$protected_epsilon == grid$protected_epsilon[i] & truth$doomed_epsilon == grid$doomed_epsilon[i] & truth$immune_epsilon == grid$immune_epsilon[i]], 3))
+    )
+    
+    result_df$additive_diff <- result_df$additive_estimate - result_df$additive_truth
+    result_df$mult_diff <- log(result_df$mult_estimate) - log(result_df$mult_truth)
+    
+    result_df$psi_1_diff <- result_df$psi_1 - result_df$psi_1_truth
+    result_df$psi_0_diff <- result_df$psi_0 - result_df$psi_0_truth
+    
+    result_df$additive_coverage <- ifelse(result_df$additive_truth >= result_df$additive_lower_ci &
+                                            result_df$additive_truth <= result_df$additive_upper_ci, 1, 0)
+    result_df$mult_coverage <- ifelse(result_df$mult_truth >= result_df$mult_lower_ci &
+                                        result_df$mult_truth <= result_df$mult_upper_ci, 1, 0)
+    
+    result_df$psi_1_coverage <- ifelse(result_df$psi_1_truth >= result_df$psi_1_lower_ci &
+                                         result_df$psi_1_truth <= result_df$psi_1_upper_ci, 1, 0)
+    result_df$psi_0_coverage <- ifelse(result_df$psi_0_truth >= result_df$psi_0_lower_ci &
+                                         result_df$psi_0_truth <= result_df$psi_0_upper_ci, 1, 0)
+    
+    all_result_df <- rbind(all_result_df, result_df)
+  }
+  
+}
+
+saveRDS(all_result_df, here::here(paste0("results/cross_fit/", setting, "_all.Rds")))
+
+# Summarize bias, variance, MSE, coverage
+summary_df <- all_result_df %>%
+  group_by(estimand, method, n) %>%
+  summarise(
+    n = n[1],
+    confirm_complete = n(),
+    bias_additive = mean(additive_diff, na.rm = TRUE),
+    var_additive = var(additive_estimate, na.rm = TRUE),
+    mse_additive = mean(additive_diff^2, na.rm = TRUE),
+    coverage_additive = mean(additive_coverage, na.rm = TRUE),
+    
+    bias_mult = mean(mult_diff, na.rm = TRUE),
+    var_mult = var(log(mult_estimate), na.rm = TRUE),
+    mse_mult = mean(mult_diff^2, na.rm = TRUE),
+    coverage_mult = mean(mult_coverage, na.rm = TRUE),
+    
+    bias_psi_1 = mean(psi_1_diff, na.rm = TRUE),
+    var_psi_1 = var(psi_1, na.rm = TRUE),
+    mse_psi_1 = mean(psi_1_diff^2, na.rm = TRUE),
+    coverage_psi_1 = mean(psi_1_coverage, na.rm = TRUE),
+    
+    
+    bias_psi_0 = mean(psi_0_diff, na.rm = TRUE),
+    var_psi_0 = var(psi_0, na.rm = TRUE),
+    mse_psi_0 = mean(psi_0_diff^2, na.rm = TRUE),
+    coverage_psi_0 = mean(psi_0_coverage, na.rm = TRUE),
+    
+    .groups = "drop"
+  )
+
+saveRDS(summary_df, paste0("results/cross_fit/", setting, "_summary.Rds"))
--- simulations/R/get_truth.R
+++ simulations/R/get_truth.R
@@ -318,6 +318,332 @@ get_truth_generic_bounds <- function(config, n = 1e7, seed = 12345){
   return(truth)
 }
 
+get_truth_generic <- function(config, n = 1e7, seed = 12345){
+  
+  grid <- expand.grid(effect_protect = config$effect_protect,
+                      doomed_inflation = as.numeric(config$doomed_inflation),
+                      protected_epsilon = as.numeric(config$protected_epsilon),
+                      doomed_epsilon = as.numeric(config$doomed_epsilon),
+                      immune_epsilon = as.numeric(config$immune_epsilon))
+  
+  truth <- cbind(grid, data.frame(E_Y1__protected_or_doomed = rep(NA, nrow(grid)),
+                                  E_Y0__protected_or_doomed = rep(NA, nrow(grid)),
+                                  E_Y1__doomed = rep(NA, nrow(grid)),
+                                  E_Y0__doomed = rep(NA, nrow(grid)),
+                                  E_Y1__protected = rep(NA, nrow(grid)),
+                                  E_Y0__protected = rep(NA, nrow(grid)),
+                                  E_Y1__pop = rep(NA, nrow(grid)),
+                                  E_Y0__pop = rep(NA, nrow(grid)),
+                                  nat_inf_upper_bound = rep(NA, nrow(grid)),
+                                  doomed_upper_bound = rep(NA, nrow(grid)),
+                                  nat_inf_upper_bound_mult = rep(NA, nrow(grid)),
+                                  doomed_upper_bound_mult = rep(NA, nrow(grid)),
+                                  nat_inf_lower_bound = rep(NA, nrow(grid)),
+                                  doomed_lower_bound = rep(NA, nrow(grid)),
+                                  nat_inf_lower_bound_mult = rep(NA, nrow(grid)),
+                                  doomed_lower_bound_mult = rep(NA, nrow(grid))))
+  
+  rhobar_v_truth <- vector("list", length = nrow(grid))
+  mubar_vs_truth <- vector("list", length = nrow(grid))
+  
+  for(i in 1:nrow(grid)){
+    big_data <- simulate_data_generic(seed = seed,
+                                      effect_protect = grid$effect_protect[i],
+                                      doomed_inflation = grid$doomed_inflation[i],
+                                      protected_epsilon = grid$protected_epsilon[i], 
+                                      doomed_epsilon = grid$doomed_epsilon[i],
+                                      immune_epsilon = grid$immune_epsilon[i],
+                                      n = n)
+    
+    # Naturally infected estimand
+    truth$E_Y1__protected_or_doomed[i] <- mean(big_data$Y1[
+        big_data$stratum %in% c("Protected", "Doomed")
+    ])
+    
+    truth$E_Y0__protected_or_doomed[i] <- mean(big_data$Y0[
+        big_data$stratum %in% c("Protected", "Doomed")
+    ])
+    
+    # Doomed estimand
+    truth$E_Y1__doomed[i] <- mean(big_data$Y1[
+        big_data$stratum %in% c("Doomed")
+    ])
+    
+    truth$E_Y0__doomed[i] <- mean(big_data$Y0[
+        big_data$stratum %in% c("Doomed")
+    ])
+    
+    # Protected estimand
+    truth$E_Y1__protected[i] <- mean(big_data$Y1[
+        big_data$stratum %in% c("Protected")
+    ])
+    
+    truth$E_Y0__protected[i] <- mean(big_data$Y0[
+        big_data$stratum %in% c("Protected")
+    ])
+    
+    # Population estimand
+    truth$E_Y1__pop[i] <- mean(big_data$Y1)
+    
+    truth$E_Y0__pop[i] <- mean(big_data$Y0)
+    
+    nat_inf_bound <- get_bound_nat_inf(big_data, Y_name = "Y", Z_name = "Z", S_name = "S", family = "binomial")
+    doomed_bound <- get_bound_doomed(big_data, Y_name = "Y", Z_name = "Z", S_name = "S", family = "binomial")
+    
+    truth$nat_inf_upper_bound[i] <- nat_inf_bound['additive_effect_upper']
+    truth$doomed_upper_bound[i] <- doomed_bound['additive_effect_upper']
+    
+    truth$nat_inf_upper_bound_mult[i] <- nat_inf_bound['mult_effect_upper']
+    truth$doomed_upper_bound_mult[i] <- doomed_bound['mult_effect_upper']
+    
+    truth$nat_inf_lower_bound[i] <- nat_inf_bound['additive_effect_lower']
+    truth$doomed_lower_bound[i] <- doomed_bound['additive_effect_lower']
+    
+    truth$nat_inf_lower_bound_mult[i] <- nat_inf_bound['mult_effect_lower']
+    truth$doomed_lower_bound_mult[i] <- doomed_bound['mult_effect_lower']
+    
+  }
+  
+  ## Point estimates effects
+  truth$effect_nat_inf <- truth$E_Y1__protected_or_doomed - truth$E_Y0__protected_or_doomed
+  truth$effect_doomed <- truth$E_Y1__doomed - truth$E_Y0__doomed
+  truth$effect_protected <- truth$E_Y1__protected - truth$E_Y0__protected
+  truth$effect_pop <- truth$E_Y1__pop - truth$E_Y0__pop
+  
+  
+  ## Point estimates effects multiplicative
+  truth$effect_nat_inf_mult <- truth$E_Y1__protected_or_doomed / truth$E_Y0__protected_or_doomed
+  truth$effect_doomed_mult <- truth$E_Y1__doomed / truth$E_Y0__doomed
+  truth$effect_protected_mult <- truth$E_Y1__protected / truth$E_Y0__protected
+  truth$effect_pop_mult <- truth$E_Y1__pop / truth$E_Y0__pop
+  
+  return(truth)
+}
+
+get_truth_generic_bounds <- function(config, n = 1e7, seed = 12345){
+  
+  grid <- expand.grid(effect_protect = config$effect_protect,
+                      doomed_inflation = as.numeric(config$doomed_inflation),
+                      protected_epsilon = as.numeric(config$protected_epsilon),
+                      doomed_epsilon = as.numeric(config$doomed_epsilon),
+                      immune_epsilon = as.numeric(config$immune_epsilon))
+  
+  # covariate combinations
+  X_combos <- c("X1", "X2", "X3",
+                "X1X2", "X2X3", "X1X3",
+                "X1X2X3")
+  
+  # all column names you want
+  cov_bound_names <- c(
+    paste0("cov_adj_upper_bound_", X_combos),
+    paste0("cov_adj_lower_bound_", X_combos),
+    paste0("cov_adj_upper_bound_mult_", X_combos),
+    paste0("cov_adj_lower_bound_mult_", X_combos)
+  )
+  
+  # baseline truth columns
+  base_names <- c(
+    "nat_inf_upper_bound",
+    "nat_inf_upper_bound_mult",
+    "nat_inf_lower_bound",
+    "nat_inf_lower_bound_mult"
+  )
+  
+  point_names <- c(
+    "E_Y1__protected_or_doomed",
+    "E_Y0__protected_or_doomed",
+    "effect_nat_inf",
+    "effect_nat_inf_mult"
+  )
+  
+  all_truth_names <- c(point_names, base_names, cov_bound_names)
+  
+  na_cols <- data.frame(
+    matrix(
+      NA_real_,
+      nrow = nrow(grid),
+      ncol = length(all_truth_names),
+      dimnames = list(NULL, all_truth_names)
+    )
+  )
+  
+  truth <- cbind(grid, na_cols)
+  
+  for(i in 1:nrow(grid)){
+    big_data <- simulate_data_generic(seed = seed,
+                                      effect_protect = grid$effect_protect[i],
+                                      doomed_inflation = grid$doomed_inflation[i],
+                                      protected_epsilon = grid$protected_epsilon[i], 
+                                      doomed_epsilon = grid$doomed_epsilon[i],
+                                      immune_epsilon = grid$immune_epsilon[i],
+                                      n = n)
+    
+    big_data$X1X2 <- as.numeric(interaction(big_data$X1, big_data$X2))
+    big_data$X2X3 <- as.numeric(interaction(big_data$X2, big_data$X3))
+    big_data$X1X3 <- as.numeric(interaction(big_data$X1, big_data$X3))
+    big_data$X1X2X3 <- as.numeric(interaction(big_data$X1, big_data$X2, big_data$X3))
+    
+    # Naturally infected estimand
+    truth$E_Y1__protected_or_doomed[i] <- mean(big_data$Y[
+      big_data$Z == 1 &
+        big_data$stratum %in% c("Protected", "Doomed")
+    ])
+    
+    truth$E_Y0__protected_or_doomed[i] <- mean(big_data$Y[
+      big_data$Z == 0 &
+        big_data$stratum %in% c("Protected", "Doomed")
+    ])
+    
+    truth$effect_nat_inf[i] <-
+      truth$E_Y1__protected_or_doomed[i] -
+      truth$E_Y0__protected_or_doomed[i]
+    
+    truth$effect_nat_inf_mult[i] <-
+      truth$E_Y1__protected_or_doomed[i] /
+      truth$E_Y0__protected_or_doomed[i]
+    
+    ## ----------------------------
+    ## Unadjusted bounds
+    ## ----------------------------
+    nat_inf_bound <- get_bound_nat_inf(
+      big_data,
+      Y_name = "Y",
+      Z_name = "Z",
+      S_name = "S",
+      family = "binomial"
+    )
+    
+    truth$nat_inf_upper_bound[i]        <- nat_inf_bound["additive_effect_upper"]
+    truth$nat_inf_lower_bound[i]        <- nat_inf_bound["additive_effect_lower"]
+    truth$nat_inf_upper_bound_mult[i]   <- nat_inf_bound["mult_effect_upper"]
+    truth$nat_inf_lower_bound_mult[i]   <- nat_inf_bound["mult_effect_lower"]
+    
+    ## ----------------------------
+    ## Covariate-adjusted bounds
+    ## ----------------------------
+    for(X in X_combos){
+      
+      cov_adj_bound <- get_cov_adj_bound_nat_inf(
+        big_data,
+        X_name = X,
+        Y_name = "Y",
+        Z_name = "Z",
+        S_name = "S",
+        family = "binomial"
+      )
+      
+      truth[i, paste0("cov_adj_upper_bound_", X)]       <- cov_adj_bound["additive_effect_upper"]
+      truth[i, paste0("cov_adj_lower_bound_", X)]       <- cov_adj_bound["additive_effect_lower"]
+      truth[i, paste0("cov_adj_upper_bound_mult_", X)]  <- cov_adj_bound["mult_effect_upper"]
+      truth[i, paste0("cov_adj_lower_bound_mult_", X)]  <- cov_adj_bound["mult_effect_lower"]
+    }
+  }
+  
+  return(truth)
+}
+
+get_truth_cross_fit <- function(config, n = 1e7, seed = 12345){
+  
+  grid <- expand.grid(effect_protect = config$effect_protect,
+                      doomed_inflation = as.numeric(config$doomed_inflation),
+                      protected_epsilon = as.numeric(config$protected_epsilon),
+                      doomed_epsilon = as.numeric(config$doomed_epsilon),
+                      immune_epsilon = as.numeric(config$immune_epsilon))
+  
+  truth <- cbind(grid, data.frame(E_Y1__protected_or_doomed = rep(NA, nrow(grid)),
+                                  E_Y0__protected_or_doomed = rep(NA, nrow(grid)),
+                                  E_Y1__doomed = rep(NA, nrow(grid)),
+                                  E_Y0__doomed = rep(NA, nrow(grid)),
+                                  E_Y1__protected = rep(NA, nrow(grid)),
+                                  E_Y0__protected = rep(NA, nrow(grid)),
+                                  E_Y1__pop = rep(NA, nrow(grid)),
+                                  E_Y0__pop = rep(NA, nrow(grid)),
+                                  nat_inf_upper_bound = rep(NA, nrow(grid)),
+                                  doomed_upper_bound = rep(NA, nrow(grid)),
+                                  nat_inf_upper_bound_mult = rep(NA, nrow(grid)),
+                                  doomed_upper_bound_mult = rep(NA, nrow(grid)),
+                                  nat_inf_lower_bound = rep(NA, nrow(grid)),
+                                  doomed_lower_bound = rep(NA, nrow(grid)),
+                                  nat_inf_lower_bound_mult = rep(NA, nrow(grid)),
+                                  doomed_lower_bound_mult = rep(NA, nrow(grid))))
+  
+  rhobar_v_truth <- vector("list", length = nrow(grid))
+  mubar_vs_truth <- vector("list", length = nrow(grid))
+  
+  for(i in 1:nrow(grid)){
+    big_data <- simulate_data_cross_fit(seed = seed,
+                                        effect_protect = grid$effect_protect[i],
+                                        doomed_inflation = grid$doomed_inflation[i],
+                                        protected_epsilon = grid$protected_epsilon[i], 
+                                        doomed_epsilon = grid$doomed_epsilon[i],
+                                        immune_epsilon = grid$immune_epsilon[i],
+                                        n = n)
+    
+    # Naturally infected estimand
+    truth$E_Y1__protected_or_doomed[i] <- mean(big_data$Y1[
+      big_data$stratum %in% c("Protected", "Doomed")
+    ])
+    
+    truth$E_Y0__protected_or_doomed[i] <- mean(big_data$Y0[
+      big_data$stratum %in% c("Protected", "Doomed")
+    ])
+    
+    # Doomed estimand
+    truth$E_Y1__doomed[i] <- mean(big_data$Y1[
+      big_data$stratum %in% c("Doomed")
+    ])
+    
+    truth$E_Y0__doomed[i] <- mean(big_data$Y0[
+      big_data$stratum %in% c("Doomed")
+    ])
+    
+    # Protected estimand
+    truth$E_Y1__protected[i] <- mean(big_data$Y1[
+      big_data$stratum %in% c("Protected")
+    ])
+    
+    truth$E_Y0__protected[i] <- mean(big_data$Y0[
+      big_data$stratum %in% c("Protected")
+    ])
+    
+    # Population estimand
+    truth$E_Y1__pop[i] <- mean(big_data$Y1)
+    
+    truth$E_Y0__pop[i] <- mean(big_data$Y0)
+    
+    nat_inf_bound <- get_bound_nat_inf(big_data, Y_name = "Y", Z_name = "Z", S_name = "S", family = "binomial")
+    doomed_bound <- get_bound_doomed(big_data, Y_name = "Y", Z_name = "Z", S_name = "S", family = "binomial")
+    
+    truth$nat_inf_upper_bound[i] <- nat_inf_bound['additive_effect_upper']
+    truth$doomed_upper_bound[i] <- doomed_bound['additive_effect_upper']
+    
+    truth$nat_inf_upper_bound_mult[i] <- nat_inf_bound['mult_effect_upper']
+    truth$doomed_upper_bound_mult[i] <- doomed_bound['mult_effect_upper']
+    
+    truth$nat_inf_lower_bound[i] <- nat_inf_bound['additive_effect_lower']
+    truth$doomed_lower_bound[i] <- doomed_bound['additive_effect_lower']
+    
+    truth$nat_inf_lower_bound_mult[i] <- nat_inf_bound['mult_effect_lower']
+    truth$doomed_lower_bound_mult[i] <- doomed_bound['mult_effect_lower']
+    
+  }
+  
+  ## Point estimates effects
+  truth$effect_nat_inf <- truth$E_Y1__protected_or_doomed - truth$E_Y0__protected_or_doomed
+  truth$effect_doomed <- truth$E_Y1__doomed - truth$E_Y0__doomed
+  truth$effect_protected <- truth$E_Y1__protected - truth$E_Y0__protected
+  truth$effect_pop <- truth$E_Y1__pop - truth$E_Y0__pop
+  
+  
+  ## Point estimates effects multiplicative
+  truth$effect_nat_inf_mult <- truth$E_Y1__protected_or_doomed / truth$E_Y0__protected_or_doomed
+  truth$effect_doomed_mult <- truth$E_Y1__doomed / truth$E_Y0__doomed
+  truth$effect_protected_mult <- truth$E_Y1__protected / truth$E_Y0__protected
+  truth$effect_pop_mult <- truth$E_Y1__pop / truth$E_Y0__pop
+  
+  return(truth)
+}
+
 # test for series of inflations to find which ones make effect size 0 for each estimand
 # config <- config::get(file = here::here("config_contour.yml"), config = "contour_plot")
 #config <- config::get(file = here::here("config_generic.yml"), config = "violate_nat_inf")
