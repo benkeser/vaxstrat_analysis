cf25fc212ee7719bb86ead81991350f1b8d5a821
Allison Codi <allison.codi@emory.edu>
2026-06-22 15:36:48 -0400

    cross fit eval update

--- simulations/R/evaluate_performance_cross_fit.R
+++ simulations/R/evaluate_performance_cross_fit.R
@@ -2,10 +2,14 @@
 # Script to evaluate performance of generic simulations 
 # ------------------------------------------------------------------------------
 
-# Path to installed packages on cluster
 options(echo = TRUE)
 
-.libPaths(c("/apps/R/4.4.0/lib64/R/site/library","/apps/R/4.4.0/lib64/R/library", "~/Rlibs_ve_trial"))
+# Path to installed packages on cluster
+.libPaths(c(
+  "/apps/R/4.4.0/lib64/R/site/library",
+  "/apps/R/4.4.0/lib64/R/library",
+  "~/Rlibs_ve_trial"
+))
 
 here::i_am("R/evaluate_performance_simulation_1.R")
 
@@ -15,11 +19,14 @@ source(here::here("R/get_truth.R"))
 devtools::load_all("~/vaxstrat")
 
 library(dplyr)
+library(purrr)
 
-# Path to projects folder where results will be saved
+# ------------------------------------------------------------------------------
+# Settings
+# ------------------------------------------------------------------------------
+
 project_dir <- "/projects/dbenkes/allison/vegrowth_analysis/results/cross_fit/"
 
-# command args config setting
 cargs <- commandArgs(trailingOnly = TRUE)
 setting <- cargs[[1]]
 
@@ -28,139 +35,229 @@ config <- cfg[[setting]]
 
 result_dir <- file.path(project_dir, setting)
 
-# Get true effects
+# ------------------------------------------------------------------------------
+# Truth
+# ------------------------------------------------------------------------------
+
 truth_file <- here::here(paste0("truth/cross_fit/", setting, "_truth.Rds"))
-if(!file.exists(truth_file)){
+
+if (!file.exists(truth_file)) {
   truth <- get_truth_cross_fit(config)
   saveRDS(truth, truth_file)
-} else{
+} else {
   truth <- readRDS(truth_file)
 }
 
-# List all matching .Rds files
-files <- list.files(
+# ------------------------------------------------------------------------------
+# Result files
+# ------------------------------------------------------------------------------
+
+files_cf <- list.files(
   path = result_dir,
-  pattern = paste0("^", setting, "_overall_seed_.*\\.Rds$"),
+  pattern = paste0("^", setting, "_cross_fit_seed_.*\\.Rds$"),
   full.names = TRUE
 )
 
-all_result_df <- data.frame()
+files_og <- list.files(
+  path = result_dir,
+  pattern = paste0("^", setting, "_single_fit_seed_.*\\.Rds$"),
+  full.names = TRUE
+)
 
-for(fname in files){
-  res_obj <- readRDS(fname)
-  seed <- as.integer(gsub(paste0(setting, "_overall_seed_|\\.Rds"), "", basename(fname)))
-  
-  grid <- expand.grid(seed = seed, 
-                      n_sample_size = as.numeric(config$n_sample_size),
-                      effect_protect = config$effect_protect,
-                      doomed_inflation = as.numeric(config$doomed_inflation),
-                      protected_epsilon = as.numeric(config$protected_epsilon),
-                      doomed_epsilon = as.numeric(config$doomed_epsilon),
-                      immune_epsilon = as.numeric(config$immune_epsilon))
-  
-  for(i in 1:nrow(grid)){
-    
-    res <- res_obj[[i]]
-    
-    result_df <- data.frame(
-      seed = seed,
-      n = grid$n_sample_size[i],
-      effect_protect = grid$effect_protect[i],
-      doomed_inflation = grid$doomed_inflation[i],
-      protected_epsilon = grid$protected_epsilon[i],
-      dooomed_epsilon = grid$doomed_epsilon[i],
-      immune_epsilon = grid$immune_epsilon[i],
-      estimand = c(rep("nat_inf",3)),
-      method = c("aipw_CW", "aipw_ER", "aipw_ER_CW"),
-      # Additive estimate and confidence interval
-      additive_estimate = c(
-        res$nat_inf$aipw_CW$pt_est['additive_effect'],
-        res$nat_inf$aipw_ER$pt_est['additive_effect'],
-        res$nat_inf$aipw_ER_CW$pt_est['additive_effect']
-      ),
-      additive_lower_ci = c(
-        res$nat_inf$aipw_CW$pt_est['additive_effect'] - 1.96*res$nat_inf$aipw_CW$pt_est['additive_se'],
-        res$nat_inf$aipw_ER$pt_est['additive_effect'] - 1.96*res$nat_inf$aipw_ER$pt_est['additive_se'],
-        res$nat_inf$aipw_ER_CW$pt_est['additive_effect'] - 1.96*res$nat_inf$aipw_ER_CW$pt_est['additive_se']),
-      additive_upper_ci = c(
-        res$nat_inf$aipw_CW$pt_est['additive_effect'] + 1.96*res$nat_inf$aipw_CW$pt_est['additive_se'],
-        res$nat_inf$aipw_ER$pt_est['additive_effect'] + 1.96*res$nat_inf$aipw_ER$pt_est['additive_se'],
-        res$nat_inf$aipw_ER_CW$pt_est['additive_effect'] + 1.96*res$nat_inf$aipw_ER_CW$pt_est['additive_se']),
-      # Multiplicative estimate and confidence interval
-      mult_estimate = c(
-        c(exp(as.numeric(res$nat_inf$aipw_CW$pt_est['log_multiplicative_effect'])),
-          exp(as.numeric(res$nat_inf$aipw_ER$pt_est['log_multiplicative_effect'])),
-          exp(as.numeric(res$nat_inf$aipw_ER_CW$pt_est['log_multiplicative_effect'])))),
-      mult_lower_ci = c(
-        exp(res$nat_inf$aipw_CW$pt_est['log_multiplicative_effect'] - 1.96*res$nat_inf$aipw_CW$pt_est['log_multiplicative_se']),
-        exp(res$nat_inf$aipw_ER$pt_est['log_multiplicative_effect'] - 1.96*res$nat_inf$aipw_ER$pt_est['log_multiplicative_se']),
-        exp(res$nat_inf$aipw_ER_CW$pt_est['log_multiplicative_effect'] - 1.96*res$nat_inf$aipw_ER_CW$pt_est['log_multiplicative_se'])),
-      mult_upper_ci = c(
-        exp(res$nat_inf$aipw_CW$pt_est['log_multiplicative_effect'] + 1.96*res$nat_inf$aipw_CW$pt_est['log_multiplicative_se']),
-        exp(res$nat_inf$aipw_ER$pt_est['log_multiplicative_effect'] + 1.96*res$nat_inf$aipw_ER$pt_est['log_multiplicative_se']),
-        exp(res$nat_inf$aipw_ER_CW$pt_est['log_multiplicative_effect'] + 1.96*res$nat_inf$aipw_ER_CW$pt_est['log_multiplicative_se'])),
-      # NEW psi_1 and psi_0
-      psi_1 = c(
-        res$nat_inf$aipw_CW$pt_est['psi_1'],
-        res$nat_inf$aipw_ER$pt_est['psi_1'],
-        res$nat_inf$aipw_ER_CW$pt_est['psi_1']),
-      psi_1_lower_ci = c(
-        res$nat_inf$aipw_CW$pt_est['psi_1'] - 1.96*res$nat_inf$aipw_CW$pt_est['se_psi_1'],
-        res$nat_inf$aipw_ER$pt_est['psi_1'] - 1.96*res$nat_inf$aipw_ER$pt_est['se_psi_1'],
-        res$nat_inf$aipw_ER_CW$pt_est['psi_1'] - 1.96*res$nat_inf$aipw_ER_CW$pt_est['se_psi_1']),
-      psi_1_upper_ci = c(
-        res$nat_inf$aipw_CW$pt_est['psi_1'] + 1.96*res$nat_inf$aipw_CW$pt_est['se_psi_1'],
-        res$nat_inf$aipw_ER$pt_est['psi_1'] + 1.96*res$nat_inf$aipw_ER$pt_est['se_psi_1'],
-        res$nat_inf$aipw_ER_CW$pt_est['psi_1'] + 1.96*res$nat_inf$aipw_ER_CW$pt_est['se_psi_1']),
-      psi_0 = c(
-        res$nat_inf$aipw_CW$pt_est['psi_0'],
-        res$nat_inf$aipw_ER$pt_est['psi_0'],
-        res$nat_inf$aipw_ER_CW$pt_est['psi_0']),
-      psi_0_lower_ci = c(
-        res$nat_inf$aipw_CW$pt_est['psi_0'] - 1.96*res$nat_inf$aipw_CW$pt_est['se_psi_0'],
-        res$nat_inf$aipw_ER$pt_est['psi_0'] - 1.96*res$nat_inf$aipw_ER$pt_est['se_psi_0'],
-        res$nat_inf$aipw_ER_CW$pt_est['psi_0'] - 1.96*res$nat_inf$aipw_ER_CW$pt_est['se_psi_0']),
-      psi_0_upper_ci = c(
-        res$nat_inf$aipw_CW$pt_est['psi_0'] + 1.96*res$nat_inf$aipw_CW$pt_est['se_psi_0'],
-        res$nat_inf$aipw_ER$pt_est['psi_0'] + 1.96*res$nat_inf$aipw_ER$pt_est['se_psi_0'],
-        res$nat_inf$aipw_ER_CW$pt_est['psi_0'] + 1.96*res$nat_inf$aipw_ER_CW$pt_est['se_psi_0']),
-      # Truth
-      additive_truth = c(rep(truth$effect_nat_inf[truth$doomed_inflation == grid$doomed_inflation[i] & truth$protected_epsilon == grid$protected_epsilon[i] & truth$doomed_epsilon == grid$doomed_epsilon[i] & truth$immune_epsilon == grid$immune_epsilon[i]], 3)),
-      mult_truth = c(rep(truth$effect_nat_inf_mult[truth$doomed_inflation == grid$doomed_inflation[i] & truth$protected_epsilon == grid$protected_epsilon[i] & truth$doomed_epsilon == grid$doomed_epsilon[i] & truth$immune_epsilon == grid$immune_epsilon[i]], 3)),
-      
-      psi_1_truth = c(rep(truth$E_Y1__protected_or_doomed[truth$doomed_inflation == grid$doomed_inflation[i] & truth$protected_epsilon == grid$protected_epsilon[i] & truth$doomed_epsilon == grid$doomed_epsilon[i] & truth$immune_epsilon == grid$immune_epsilon[i]], 3)), 
-      psi_0_truth = c(rep(truth$E_Y0__protected_or_doomed[truth$doomed_inflation == grid$doomed_inflation[i] & truth$protected_epsilon == grid$protected_epsilon[i] & truth$doomed_epsilon == grid$doomed_epsilon[i] & truth$immune_epsilon == grid$immune_epsilon[i]], 3))
-    )
-    
-    result_df$additive_diff <- result_df$additive_estimate - result_df$additive_truth
-    result_df$mult_diff <- log(result_df$mult_estimate) - log(result_df$mult_truth)
-    
-    result_df$psi_1_diff <- result_df$psi_1 - result_df$psi_1_truth
-    result_df$psi_0_diff <- result_df$psi_0 - result_df$psi_0_truth
-    
-    result_df$additive_coverage <- ifelse(result_df$additive_truth >= result_df$additive_lower_ci &
-                                            result_df$additive_truth <= result_df$additive_upper_ci, 1, 0)
-    result_df$mult_coverage <- ifelse(result_df$mult_truth >= result_df$mult_lower_ci &
-                                        result_df$mult_truth <= result_df$mult_upper_ci, 1, 0)
-    
-    result_df$psi_1_coverage <- ifelse(result_df$psi_1_truth >= result_df$psi_1_lower_ci &
-                                         result_df$psi_1_truth <= result_df$psi_1_upper_ci, 1, 0)
-    result_df$psi_0_coverage <- ifelse(result_df$psi_0_truth >= result_df$psi_0_lower_ci &
-                                         result_df$psi_0_truth <= result_df$psi_0_upper_ci, 1, 0)
-    
-    all_result_df <- rbind(all_result_df, result_df)
+message("Found ", length(files_cf), " cross-fit files")
+message("Found ", length(files_og), " single-fit/original files")
+
+# ------------------------------------------------------------------------------
+# Helpers
+# ------------------------------------------------------------------------------
+
+get_seed <- function(fname, setting, cross_fit) {
+  if (cross_fit) {
+    as.integer(gsub(paste0(setting, "_cross_fit_seed_|\\.Rds"), "", basename(fname)))
+  } else {
+    as.integer(gsub(paste0(setting, "_single_fit_seed_|\\.Rds"), "", basename(fname)))
   }
-  
 }
 
-saveRDS(all_result_df, here::here(paste0("results/cross_fit/", setting, "_all.Rds")))
+make_grid <- function(seed, config) {
+  expand.grid(
+    seed = seed,
+    n_sample_size = as.numeric(config$n_sample_size),
+    effect_protect = config$effect_protect,
+    doomed_inflation = as.numeric(config$doomed_inflation),
+    protected_epsilon = as.numeric(config$protected_epsilon),
+    doomed_epsilon = as.numeric(config$doomed_epsilon),
+    immune_epsilon = as.numeric(config$immune_epsilon)
+  )
+}
 
+match_truth_row <- function(truth, grid_row) {
+  truth %>%
+    filter(
+      doomed_inflation == grid_row$doomed_inflation,
+      protected_epsilon == grid_row$protected_epsilon,
+      doomed_epsilon == grid_row$doomed_epsilon,
+      immune_epsilon == grid_row$immune_epsilon
+    )
+}
+
+extract_method_result <- function(res, method) {
+  res$nat_inf[[method]]$pt_est
+}
+
+make_result_df_one_grid <- function(res, grid_row, truth, cross_fit) {
+  
+  methods <- c("aipw_CW", "aipw_ER", "aipw_ER_CW")
+  tr <- match_truth_row(truth, grid_row)
+  
+  if (nrow(tr) != 1) {
+    stop(
+      "Truth match did not return exactly one row for seed ",
+      grid_row$seed,
+      ", n = ",
+      grid_row$n_sample_size
+    )
+  }
+  
+  out <- lapply(methods, function(method) {
+    
+    pt <- extract_method_result(res, method)
+    
+    additive_estimate <- as.numeric(pt["additive_effect"])
+    additive_se <- as.numeric(pt["additive_se"])
+    
+    log_mult_estimate <- as.numeric(pt["log_multiplicative_effect"])
+    log_mult_se <- as.numeric(pt["log_multiplicative_se"])
+    
+    psi_1 <- as.numeric(pt["psi_1"])
+    se_psi_1 <- as.numeric(pt["se_psi_1"])
+    
+    psi_0 <- as.numeric(pt["psi_0"])
+    se_psi_0 <- as.numeric(pt["se_psi_0"])
+    
+    data.frame(
+      seed = grid_row$seed,
+      cross_fit = cross_fit,
+      n = grid_row$n_sample_size,
+      effect_protect = grid_row$effect_protect,
+      doomed_inflation = grid_row$doomed_inflation,
+      protected_epsilon = grid_row$protected_epsilon,
+      doomed_epsilon = grid_row$doomed_epsilon,
+      immune_epsilon = grid_row$immune_epsilon,
+      
+      estimand = "nat_inf",
+      method = method,
+      
+      # Additive estimate and CI
+      additive_estimate = additive_estimate,
+      additive_lower_ci = additive_estimate - 1.96 * additive_se,
+      additive_upper_ci = additive_estimate + 1.96 * additive_se,
+      
+      # Multiplicative estimate and CI
+      mult_estimate = exp(log_mult_estimate),
+      mult_lower_ci = exp(log_mult_estimate - 1.96 * log_mult_se),
+      mult_upper_ci = exp(log_mult_estimate + 1.96 * log_mult_se),
+      
+      # psi_1
+      psi_1 = psi_1,
+      psi_1_lower_ci = psi_1 - 1.96 * se_psi_1,
+      psi_1_upper_ci = psi_1 + 1.96 * se_psi_1,
+      
+      # psi_0
+      psi_0 = psi_0,
+      psi_0_lower_ci = psi_0 - 1.96 * se_psi_0,
+      psi_0_upper_ci = psi_0 + 1.96 * se_psi_0,
+      
+      # Truth
+      additive_truth = tr$effect_nat_inf,
+      mult_truth = tr$effect_nat_inf_mult,
+      psi_1_truth = tr$E_Y1__protected_or_doomed,
+      psi_0_truth = tr$E_Y0__protected_or_doomed
+    )
+  })
+  
+  bind_rows(out) %>%
+    mutate(
+      additive_diff = additive_estimate - additive_truth,
+      mult_diff = log(mult_estimate) - log(mult_truth),
+      psi_1_diff = psi_1 - psi_1_truth,
+      psi_0_diff = psi_0 - psi_0_truth,
+      
+      additive_coverage = as.integer(
+        additive_truth >= additive_lower_ci &
+          additive_truth <= additive_upper_ci
+      ),
+      mult_coverage = as.integer(
+        mult_truth >= mult_lower_ci &
+          mult_truth <= mult_upper_ci
+      ),
+      psi_1_coverage = as.integer(
+        psi_1_truth >= psi_1_lower_ci &
+          psi_1_truth <= psi_1_upper_ci
+      ),
+      psi_0_coverage = as.integer(
+        psi_0_truth >= psi_0_lower_ci &
+          psi_0_truth <= psi_0_upper_ci
+      )
+    )
+}
+
+evaluate_file <- function(fname, setting, config, truth, cross_fit) {
+  
+  res_obj <- readRDS(fname)
+  seed <- get_seed(fname, setting, cross_fit)
+  grid <- make_grid(seed, config)
+  
+  if (length(res_obj) != nrow(grid)) {
+    stop(
+      "Length mismatch in file: ", basename(fname),
+      ". length(res_obj) = ", length(res_obj),
+      ", nrow(grid) = ", nrow(grid)
+    )
+  }
+  
+  bind_rows(lapply(seq_len(nrow(grid)), function(i) {
+    make_result_df_one_grid(
+      res = res_obj[[i]],
+      grid_row = grid[i, ],
+      truth = truth,
+      cross_fit = cross_fit
+    )
+  }))
+}
+
+# ------------------------------------------------------------------------------
+# Evaluate all results
+# ------------------------------------------------------------------------------
+
+all_result_df <- bind_rows(
+  lapply(files_cf, evaluate_file,
+         setting = setting,
+         config = config,
+         truth = truth,
+         cross_fit = TRUE),
+  
+  lapply(files_og, evaluate_file,
+         setting = setting,
+         config = config,
+         truth = truth,
+         cross_fit = FALSE)
+)
+
+saveRDS(
+  all_result_df,
+  here::here(paste0("results/cross_fit/", setting, "_all.Rds"))
+)
+
+# ------------------------------------------------------------------------------
 # Summarize bias, variance, MSE, coverage
+# ------------------------------------------------------------------------------
+
 summary_df <- all_result_df %>%
-  group_by(estimand, method, n) %>%
+  group_by(cross_fit, estimand, method, n) %>%
   summarise(
-    n = n[1],
     confirm_complete = n(),
+    
     bias_additive = mean(additive_diff, na.rm = TRUE),
     var_additive = var(additive_estimate, na.rm = TRUE),
     mse_additive = mean(additive_diff^2, na.rm = TRUE),
@@ -176,7 +273,6 @@ summary_df <- all_result_df %>%
     mse_psi_1 = mean(psi_1_diff^2, na.rm = TRUE),
     coverage_psi_1 = mean(psi_1_coverage, na.rm = TRUE),
     
-    
     bias_psi_0 = mean(psi_0_diff, na.rm = TRUE),
     var_psi_0 = var(psi_0, na.rm = TRUE),
     mse_psi_0 = mean(psi_0_diff^2, na.rm = TRUE),
@@ -185,4 +281,7 @@ summary_df <- all_result_df %>%
     .groups = "drop"
   )
 
-saveRDS(summary_df, paste0("results/cross_fit/", setting, "_summary.Rds"))
+saveRDS(
+  summary_df,
+  here::here(paste0("results/cross_fit/", setting, "_summary.Rds"))
+)
